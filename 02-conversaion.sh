97abb90c87eea85774428289006c534bc11cdb52
karthikmacharla2025-cell <karthikmacharla2025@gmail.com>
2026-05-28 11:38:14 +0530

    hello world

--- 02-conversaion.sh
+++ 02-conversaion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=Trump
-PERSON2=Iran
+PERSON1=Russia
+PERSON2=Ukraine
 
 echo "$PERSON1: hey $PERSON2, we are going to blast you!"
 echo "$PERSON2: yes, please. you are welcome"
