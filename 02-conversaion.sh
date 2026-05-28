e539881e3825a868b2adb062e375cd30c7367148
karthikmacharla2025-cell <karthikmacharla2025@gmail.com>
2026-05-28 11:23:51 +0530

    hello world

---
+++ 02-conversaion.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Trump: hey Iran, we are going to blast you!"
+echo "Iran: yes, please. you are welcome"
+echo "Trump: Hey Iran i am serious"
+echo "Iran: Hey Trump, we are also serious" 
