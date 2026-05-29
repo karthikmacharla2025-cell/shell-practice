24d0690c414f71c3c987b2939b43f06849998250
karthikmacharla2025-cell <karthikmacharla2025@gmail.com>
2026-05-29 18:56:26 +0530

    day2

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -2,7 +2,7 @@
 
 NUMBER=$1
 
-if [ $NUMBER -ge 20 ]; then
+if [ $NUMBER -gt 20 ]; then
     echo "Given numer $NUMBER is greater than or equal to 20"
 elif [ $NUMBER -eq 20 ]; then
     echo "Given numer $NUMBER is equal to 20"
